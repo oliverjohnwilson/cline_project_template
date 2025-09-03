e8a3003ecce5c1534c61af8750e3cece02949a2a
Oliver <34634440+oliverjohnwilson@users.noreply.github.com>
2025-09-02 22:33:13 -0500

    Create main.py

---
+++ src/cline_project_template/cli/main.py
@@ -0,0 +1,22 @@
+"""
+Main CLI entrypoint.
+
+Author: Oliver J Wilson
+"""
+
+import click
+from ..adapters.cli_adapter import main as example_main
+
+
+@click.group()
+def cli() -> None:
+    """cline_project_template CLI."""
+    pass
+
+
+# Register subcommands
+cli.add_command(example_main, name="example")
+
+
+if __name__ == "__main__":
+    cli()
